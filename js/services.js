55fcaa24864fa56253129acadb3c9f07d863f8c7
Taylor King <dekkofilms@gmail.com>
2016-11-06 11:12:53 -0600

    cart object now counts each item added with a new ID

--- js/services.js
+++ js/services.js
@@ -1,13 +1,12 @@
 app.service('checkout', function () {
   this.checkoutBag = [];
 
+  var countID = 0
   this.addToBag = function (image, name, price, ingredients, id, quantity) {
-    console.log(id);
-    console.log(typeof parseInt(quantity));
-    console.log(quantity);
 
     if (parseInt(quantity) > 0) {
       let newItem = {
+        'id' : countID,
         'teaId' : id,
         'name' : name,
         'imageUrl' : image,
@@ -16,8 +15,8 @@ app.service('checkout', function () {
         'ingredients' : ingredients,
       }
       this.checkoutBag.push(newItem);
+      countID++;
     }
-
     console.log(this.checkoutBag);
   };
 });
