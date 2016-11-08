d2c0f482283e81a91749926308c291b8258a8f65
Taylor King <dekkofilms@gmail.com>
2016-11-08 12:13:39 -0600

    gauge animation works

--- js/directives.js
+++ js/directives.js
@@ -18,7 +18,33 @@ app.directive('caffeinemeter', function () {
       if ($scope.scale < 80) {
         $scope.rating = 'LOW';
       };
-      
+
+      var gaugeReading = Math.floor(($scope.scale)/1.6)
+      var gaugeElements = '<div id="' + $scope.id + '" class="gauge">\
+                            <div class="mask">\
+                              <div class="semi-circle"></div>\
+                              <div class="semi-circle--mask"></div>\
+                            </div>\
+                          </div>'
+
+      var lastGaugeSectionIndex = $(".gauge-section").length - 1
+      var selectedGaugeSection = $(".gauge-section")[lastGaugeSectionIndex]
+      $(selectedGaugeSection).append(gaugeElements);
+
+      var selectedMeter = $("#" + $scope.id + " .semi-circle--mask")
+
+      selectedMeter.animate({ textIndent: gaugeReading },
+        {
+          step: function(now,fx){
+            $(this).css({
+              '-webkit-transform': 'rotate('+ now +'deg)',
+              '-moz-transform': 'rotate('+ now +'deg)',
+              '-ms-transform': 'rotate('+ now +'deg)',
+              'transform': 'rotate('+ now +'deg)'
+            });
+          },
+          duration: 1000
+        })
     },
   }
 });
